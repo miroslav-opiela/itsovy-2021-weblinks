e8d7b747bc841ce5b8a1fae08ee97dd58f024704
miroslav-opiela <miroslav-opiela@upjs.sk>
2021-10-15 09:32:20 +0200

    vlastna trieda na adapter

--- app/src/main/java/sk/itsovy/android/weblinks/MainActivity.kt
+++ app/src/main/java/sk/itsovy/android/weblinks/MainActivity.kt
@@ -2,10 +2,19 @@ package sk.itsovy.android.weblinks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
+        //val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        recyclerView.adapter = WeblinksAdapter()
     }
 }
---
+++ app/src/main/java/sk/itsovy/android/weblinks/WeblinksAdapter.kt
@@ -0,0 +1,26 @@
+package sk.itsovy.android.weblinks
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class WeblinksAdapter : RecyclerView.Adapter<WeblinksAdapter.WeblinksViewHolder>() {
+
+
+    class WeblinksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeblinksViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: WeblinksViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
