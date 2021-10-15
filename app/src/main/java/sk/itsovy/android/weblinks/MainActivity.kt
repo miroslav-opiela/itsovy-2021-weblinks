4b9c85213b48d766c28e211bf6c1fdef63bc25b2
miroslav-opiela <miroslav-opiela@upjs.sk>
2021-10-15 12:50:55 +0200

    intent otvorenie wikipedie

--- app/src/main/java/sk/itsovy/android/weblinks/MainActivity.kt
+++ app/src/main/java/sk/itsovy/android/weblinks/MainActivity.kt
@@ -1,5 +1,7 @@
 package sk.itsovy.android.weblinks
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -21,6 +23,12 @@ class MainActivity : AppCompatActivity(), OnWeblinkClickListener {
     }
 
     override fun onWeblinkClick(weblink: Weblink) {
-        Toast.makeText(this, weblink.toString(), Toast.LENGTH_LONG).show()
+        //Toast.makeText(this, weblink.toString(), Toast.LENGTH_LONG).show()
+
+        // alebo to vlozit priamo v konstruktore
+        val intent = Intent()
+        intent.action = Intent.ACTION_VIEW
+        intent.data = Uri.parse(weblink.url)
+        startActivity(intent)
     }
 }
