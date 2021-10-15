723a6e25dfef2c2bf0edbe7a22027b735c92a032
miroslav-opiela <miroslav-opiela@upjs.sk>
2021-10-15 10:02:38 +0200

    recycler view s listom stringov

--- app/src/main/java/sk/itsovy/android/weblinks/WeblinksAdapter.kt
+++ app/src/main/java/sk/itsovy/android/weblinks/WeblinksAdapter.kt
@@ -1,26 +1,37 @@
 package sk.itsovy.android.weblinks
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class WeblinksAdapter : RecyclerView.Adapter<WeblinksAdapter.WeblinksViewHolder>() {
 
+    val weblinksTitles = listOf(
+        "Kaikhosru Shapurji Sorabji",
+        "Primeira Idade",
+        "Lynda Adams",
+        "Ray Leatherwood",
+        "Bawlte Rohmingthanga"
+    )
 
     class WeblinksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        // hladam widget v ramci layoutu jednej polozky, ktory pride ako parameter
+        val textView: TextView = itemView.findViewById(R.id.textViewWeblinkTitle)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeblinksViewHolder {
-        TODO("Not yet implemented")
+        val layout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.weblink_item_layout, parent, false)
+        return WeblinksViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: WeblinksViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.textView.text = weblinksTitles[position]
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    // vrati pocet poloziek v zozname - mnozstvo dat
+    override fun getItemCount(): Int = weblinksTitles.size
 
 }
