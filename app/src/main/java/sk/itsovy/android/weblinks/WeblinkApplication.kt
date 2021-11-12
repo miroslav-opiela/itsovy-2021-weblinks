16da12ba7efd0828bbac2250dd5bb5e02108b0b6
miroslav-opiela <miroslav-opiela@upjs.sk>
2021-11-12 09:42:33 +0100

    populate database

--- app/src/main/java/sk/itsovy/android/weblinks/WeblinkApplication.kt
+++ app/src/main/java/sk/itsovy/android/weblinks/WeblinkApplication.kt
@@ -1,14 +1,18 @@
 package sk.itsovy.android.weblinks
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 // aplikacia je globalna vec dostupna zo vsetkych aktivit
 class WeblinkApplication : Application() {
 
+    val applicationScope = CoroutineScope(SupervisorJob())
+
     // by lazy je delegovane - lambda funkcia vo vnutri sa vola iba prvykrat, potom sa uz vrati vypocitany vysledok
     // teda mame iba jeden vyskyt database a repository
     // databaza sa vyrobi pomocou statickej funkcie
-    val database by lazy { WeblinkRoomDatabase.getDatabase(this) }
+    val database by lazy { WeblinkRoomDatabase.getDatabase(this, applicationScope) }
     // repository sa vyrobi novy objekt - v kotline slovo new nepiseme
     val repository by lazy { WeblinkRepository(database.weblinkDao()) }
 
--- app/src/main/java/sk/itsovy/android/weblinks/WeblinkRoomDatabase.kt
+++ app/src/main/java/sk/itsovy/android/weblinks/WeblinkRoomDatabase.kt
@@ -5,6 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [Weblink::class], version = 1, exportSchema = false)
 @TypeConverters(UuidConverters::class)
@@ -17,20 +20,43 @@ public abstract class WeblinkRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WeblinkRoomDatabase? = null
 
-        fun getDatabase(context: Context): WeblinkRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): WeblinkRoomDatabase {
             return INSTANCE ?: synchronized(this) {
                 // vyrobim novu instanciu databazy
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     WeblinkRoomDatabase::class.java,
                     "weblink_database"
-                ).build()
+                ).addCallback(WeblinkDatabaseCallback(scope))
+                    .build()
                 // vlozim do inst. premennej
                 INSTANCE = instance
                 // vratim vysledok
                 instance
             }
         }
+    }
+
+    private class WeblinkDatabaseCallback(private val scope: CoroutineScope) :
+        RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.weblinkDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(dao: WeblinkDao) {
+            dao.deleteAll()
+
+            dao.insert(Weblink("Kosice", 3))
+            dao.insert(Weblink("Wikipedia", 2))
+            dao.insert(Weblink("Slovakia", 0))
+        }
+
     }
 
 }
